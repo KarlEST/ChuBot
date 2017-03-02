aa71bfcd97dc1243a7c69c61fe9ab3824304e76a
Karl Lääts <karl.l22ts@hotmailc.om>
2017-03-02 10:51:29 +0200

    remove config.js

--- config.js
+++ config.js
@@ -1,9 +1,3 @@
 export default {
-	token: 'xoxb-131403324868-yYMS9WJmAvc8SdtvK74ACR2J',
-	name: 'ChuBot',
-	picture: 'http://avatars.slack-edge.com/2017-01-23/131407495044_16ae2585197d7facceca_48.png',
-	facebook: {
-		id: '953704794759613',
-		secret: '706d613aa7236a5f429d5a4f4596eb9d'
-	}
+
 };
